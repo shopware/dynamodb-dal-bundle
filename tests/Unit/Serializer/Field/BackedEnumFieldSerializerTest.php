68c3019f4dae5590ed7141239f9b2616c8f81b6e
Michel <m.bade@shopware.com>
2026-09-24 11:48:04 +0200

    fix: int backed enums (#10)

--- tests/Unit/Serializer/Field/BackedEnumFieldSerializerTest.php
+++ tests/Unit/Serializer/Field/BackedEnumFieldSerializerTest.php
@@ -10,6 +10,7 @@ use Shopware\DynamodbDalBundle\Exception\MissingAttributeValueException;
 use Shopware\DynamodbDalBundle\Serializer\Field\BackedEnumFieldSerializer;
 use AsyncAws\DynamoDb\ValueObject\AttributeValue;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(BackedEnumFieldSerializer::class)]
@@ -78,9 +79,44 @@ class BackedEnumFieldSerializerTest extends TestCase
         $this->serializer->deserialize($this->definition, $attribute);
     }
 
-    private function createFieldDefinition(): FieldDefinition
+    public function testDeserializeThrowsForAValueThatIsNoLongerACaseOfAStringBackedEnum(): void
     {
-        $definition = new FieldDefinition('status', GoodEnum::class, false, false, null, $this->serializer);
+        $definition = $this->createFieldDefinition(LabelEnum::class);
+
+        $this->expectException(\ValueError::class);
+
+        $this->serializer->deserialize($definition, AttributeValue::create(['S' => 'retired']));
+    }
+
+    /**
+     * @return iterable<string, array{string}>
+     */
+    public static function nonIntegerStrings(): iterable
+    {
+        yield 'a decimal' => ['1.5'];
+        yield 'digits followed by text' => ['2abc'];
+        yield 'text, which (int) makes 0' => ['abc'];
+    }
+
+    /**
+     * `(int)` would read each of these as a case: 1, 2 and 0.
+     */
+    #[DataProvider('nonIntegerStrings')]
+    public function testDeserializeThrowsForAStringThatIsNotExactlyAnIntegerOfAnIntBackedEnum(string $value): void
+    {
+        $definition = $this->createFieldDefinition(ZeroEnum::class);
+
+        $this->expectExceptionObject(new \ValueError(\sprintf('"%s" is not a valid backing value for enum %s', $value, ZeroEnum::class)));
+
+        $this->serializer->deserialize($definition, AttributeValue::create(['S' => $value]));
+    }
+
+    /**
+     * @param class-string<\BackedEnum> $enum
+     */
+    private function createFieldDefinition(string $enum = GoodEnum::class): FieldDefinition
+    {
+        $definition = new FieldDefinition('status', $enum, false, false, null, $this->serializer);
         $definition->setEntityDefinition(new EntityDefinition('order', 'order', OrderEntity::class, null, [], new KeySchema('tenantId')));
 
         return $definition;
@@ -95,4 +131,16 @@ enum GoodEnum: int
 
 enum EmptyEnum: int
 {
+}
+
+enum LabelEnum: string
+{
+    case Open = 'open';
+}
+
+enum ZeroEnum: int
+{
+    case None = 0;
+    case One = 1;
+    case Two = 2;
 }
